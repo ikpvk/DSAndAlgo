5ea8f47ca6bcb4031dd0f8580799229ba11bf90d
Krishna Prasad <krishnaprasad.vk@gmail.com>
2020-08-22 23:53:53 +0530

    Adding circular linked list files || Refactoring

--- src/ds/circularlnkedlist/Node.java
+++ src/ds/circularlnkedlist/Node.java
@@ -4,13 +4,8 @@ public class Node {
     public int data;
     public Node next;
 
-	Node(int data) {
-		this.data = data;
-	}
-
-    Node(int data, Node next) {
+    Node(int data) {
         this.data = data;
-        this.next = next;
     }
 
     public void displayNode() {
