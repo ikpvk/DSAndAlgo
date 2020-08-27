f7f742c5abe67ede7c9a0d82b709050812f9c9e2
Krishna Prasad <krishnaprasad.vk@gmail.com>
2020-08-28 00:08:53 +0530

    Binary Search Tree || In Progress

--- src/ds/binarysearchtree/App.java
+++ src/ds/binarysearchtree/App.java
@@ -3,6 +3,12 @@ package ds.binarysearchtree;
 public class App {
     public static void main(String[] args) {
         BST bst = new BST();
-
+        bst.findMax();
+        bst.findMin();
+        bst.insert(10,"Ten");
+        bst.insert(20,"Twenty");
+        bst.insert(5,"Five");
+        bst.findMax();
+        bst.findMin();
     }
 }
--- src/ds/binarysearchtree/BST.java
+++ src/ds/binarysearchtree/BST.java
@@ -9,20 +9,20 @@ public class BST {
 
     public void insert(int key, String value) {
         Node newNode = new Node(key, value);
-        if(isEmpty()) {
+        if (isEmpty()) {
             root = newNode;
         } else {
             Node traverser = root;
-            while(true) {
-                if(key < traverser.key) {
-                    if(null == traverser.leftChild) {
+            while (true) {
+                if (key < traverser.key) {
+                    if (null == traverser.leftChild) {
                         traverser.leftChild = newNode;
                         break;
                     } else {
                         traverser = traverser.leftChild;
                     }
                 } else {
-                    if(null == traverser.rightChild) {
+                    if (null == traverser.rightChild) {
                         traverser.rightChild = newNode;
                         break;
                     } else {
@@ -30,6 +30,31 @@ public class BST {
                     }
                 }
             }
+        }
+        System.out.println("New node {" + newNode.key + ":" + newNode.value + "} inserted");
+    }
+
+    public void findMin() {
+        if (isEmpty()) {
+            System.out.println("Empty BST");
+        } else {
+            Node traverser = root;
+            while (null != traverser.leftChild) {
+                traverser = traverser.leftChild;
+            }
+            System.out.println("Min node : {" + traverser.key + ":" + traverser.value + "}");
+        }
+    }
+
+    public void findMax() {
+        if (isEmpty()) {
+            System.out.println("Empty BST");
+        } else {
+            Node traverser = root;
+            while (null != traverser.rightChild) {
+                traverser = traverser.rightChild;
+            }
+            System.out.println("Max node : {" + traverser.key + ":" + traverser.value + "}");
         }
     }
 }
