15592f148202d2b5e337f0167f6a22d431988b0b
Krishna Prasad <krishnaprasad.vk@gmail.com>
2022-06-16 23:15:38 +0530

    Adding code format

--- src/algos/mergesort/Solution.java
+++ src/algos/mergesort/Solution.java
@@ -16,25 +16,25 @@ public class Solution {
     public static void mergeArrays(int arr[], int leftStart, int rightEnd, int mid, int[] sortedArr) {
         int startIndex = leftStart, endIndex = rightEnd;
         int leftEnd = mid;
-        int rightStart = mid+1;
+        int rightStart = mid + 1;
         int index = leftStart;
-        while(leftStart<=leftEnd && rightStart <=rightEnd) {
-            if(arr[leftStart]<= arr[rightStart]) {
+        while (leftStart <= leftEnd && rightStart <= rightEnd) {
+            if (arr[leftStart] <= arr[rightStart]) {
                 sortedArr[index++] = arr[leftStart++];
             } else {
                 sortedArr[index++] = arr[rightStart++];
             }
         }
 
-        while(leftStart<=leftEnd) {
-                sortedArr[index++] = arr[leftStart++];
+        while (leftStart <= leftEnd) {
+            sortedArr[index++] = arr[leftStart++];
         }
 
-        while(rightStart <=rightEnd) {
-                sortedArr[index++] = arr[rightStart++];
+        while (rightStart <= rightEnd) {
+            sortedArr[index++] = arr[rightStart++];
         }
 
-        while(startIndex<=endIndex) {
+        while (startIndex <= endIndex) {
             arr[startIndex] = sortedArr[startIndex++];
         }
     }
